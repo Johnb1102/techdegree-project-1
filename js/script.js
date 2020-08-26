035347b1a26c798cf4248b4cd6e12a5866a2dc2e
John Bruno <johnbruno1260@gmail.com>
2020-08-26 12:04:04 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -11,11 +11,56 @@ project 1 - A Random Quote Generator
  * `quotes` array 
 ***/
 
+let quotes=[
 
+  {
+    quote: "I was born to dictate, never follow orders",
+    name: "Jay-Z",
+    song: "U Don't Know",
+    year: 2001,
+  },
+
+  {
+    quote: "A man that don't take care his family can't be rich",
+    name: "Jay-Z",
+    song: "Family Feud",
+    year: 2017,
+  },
+
+  {
+    quote: "Generational wealth, that's the key",
+    name: "Jay-Z",
+    song: "Legacy",
+    year: 2017,
+  },
+  {
+    quote: "Nobody built like you, you design yourself",
+    name: "Jay-Z",
+    song: "A Dream",
+    year: 2002,
+  },
+  {
+    quote: "Standing back from situations, gives you the perfect view",
+    name: "Jay-Z",
+    song: "Anything",
+    year: 1999,
+  },
+
+
+]
+ 
+  
 
 /***
  * `getRandomQuote` function
 ***/
+
+function getRandomQuote() {
+ let randomNumber = Math.floor(Math.random() * (quotes.length));
+return quotes [randomNumber];
+
+}
+  
 
 
 
@@ -23,6 +68,10 @@ project 1 - A Random Quote Generator
  * `printQuote` function
 ***/
 
+function printQuote() {
+ 
+
+}
 
 
 /***
